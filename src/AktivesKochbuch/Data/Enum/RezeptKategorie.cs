d2fba240a4fbe935317a0fecc47fa1ca3547a455
Jan <janan9980@gmail.com>
2023-10-23 00:00:53 +0200

    Rezept Model erstellt

---
+++ src/AktivesKochbuch/Data/Enum/RezeptKategorie.cs
@@ -0,0 +1,10 @@
+namespace AktivesKochbuch.Data.Enum;
+
+public enum RezeptKategorie
+{
+    Snack,
+    Frühstück,
+    Hauptmahlzeit,
+    Dessert,
+    Vorspeise
+}
--- src/AktivesKochbuch/Models/Rezept.cs
+++ src/AktivesKochbuch/Models/Rezept.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using AktivesKochbuch.Data.Enum;
+
 namespace AktivesKochbuch.Models;
 
 public class Rezept
 {
-    
+    [Key] 
+    public int Id { get; set; }
+    public string RezeptTitel { get; set; }
+    public string Zubereitung { get; set; }
+    public RezeptKategorie RezeptKategorie { get; set; }
 }
