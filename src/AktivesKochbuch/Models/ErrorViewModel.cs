5b5902dcf59d81b42a95bed031fcd63cdefadab2
Jan <janan9980@gmail.com>
2023-10-22 23:57:05 +0200

    Initial Commit

---
+++ src/AktivesKochbuch/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AktivesKochbuch.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
