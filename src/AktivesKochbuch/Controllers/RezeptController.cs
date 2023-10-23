bd83ea619d458044e8e2bad119d29f44bfda4e02
Jan <janan9980@gmail.com>
2023-10-23 12:40:18 +0200

    (RezeptController) Rezepte in View eingefügt

--- src/AktivesKochbuch/Controllers/RezeptController.cs
+++ src/AktivesKochbuch/Controllers/RezeptController.cs
@@ -6,14 +6,15 @@ namespace AktivesKochbuch.Controllers;
 public class RezeptController : Controller
 {
     private readonly ApplicationDbContext _context;
-
+    
     public RezeptController(ApplicationDbContext context)
     {
         _context = context;
     }
+    
     public IActionResult Index()
     {
         var rezepte = _context.Rezepte.ToList();
-        return View();
+        return View(rezepte);
     }
 }
