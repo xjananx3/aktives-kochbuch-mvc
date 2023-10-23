3f1f57fae477808269dad44406db294ef4cc24ea
Jan <janan9980@gmail.com>
2023-10-23 12:28:09 +0200

    (RezeptController)ApplicationDbContext eingeführt

--- src/AktivesKochbuch/Controllers/RezeptController.cs
+++ src/AktivesKochbuch/Controllers/RezeptController.cs
@@ -1,12 +1,19 @@
+using AktivesKochbuch.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AktivesKochbuch.Controllers;
 
 public class RezeptController : Controller
 {
-    // GET
+    private readonly ApplicationDbContext _context;
+
+    public RezeptController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
+        var rezepte = _context.Rezepte.ToList();
         return View();
     }
 }
